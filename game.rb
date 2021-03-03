d1e025243f22359e0ef8a2bf098a4c3725dd89ed
Rafin Reza <rafintokyo@gmail.com>
2021-03-03 00:03:20 +0000

    create game plan, finish question class

---
+++ game.rb
@@ -0,0 +1,4 @@
+## Create player class, each player has 3 lives that are tracked
+## Current player is tracked between the two players, either true of false
+## Player 1 will always go first (true), player 2 will go after (initially false)
+## Players can lose lives if they answer the question wrong.
---
+++ question.rb
@@ -0,0 +1,14 @@
+# Pick two numbers at random b/w 1 - 20
+# Create a simple addition q with those two numbers
+# Check answer
+
+class question
+  attr_reader :newQ, :ans
+
+  def initialize
+    num1 = rand(1..20)
+    num2 = rand(1..20)
+    @newQ = "What does #{num1} plus #{num2} equal?"
+    @ans = num1 + num2
+  end
+end
